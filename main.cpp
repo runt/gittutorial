0c6c9c3dd5ce92756ed39ded00e362d91bdd89c9
Runt Josef PLN DIWTW42 <josef.runt@zf.com>
2023-03-08 09:39:06 +0000

    first release of tests

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int promenna{5};
+    std::cout << " Hello world \n";
+    std::cout << " promenna = " << promenna << " \n";
+    std::cout << " adresa promenne = " << &promenna << " \n";
+    return 0;
+}
