6897572820bc10a806a55034474fceb642e8fb56
Runt Josef PLN DIWTW42 <josef.runt@zf.com>
2023-03-08 10:42:37 +0100

    added line to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int promenna{5};
     std::cout << " Hello world \n";
+    std::cout << " Hello world 2\n";
     std::cout << " promenna = " << promenna << " \n";
     std::cout << " adresa promenne = " << &promenna << " \n";
     return 0;
