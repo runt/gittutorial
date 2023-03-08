d5d0469e1628885375df0558b95cec15888f81be
Runt Josef PLN DIWTW42 <josef.runt@zf.com>
2023-03-08 10:03:31 +0000

    rychl fix, pridan radek Hello world 3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
     int promenna{5};
     std::cout << " Hello world \n";
     std::cout << " Hello world 2\n";
+    std::cout << " Hello world 3\n";
     std::cout << " promenna = " << promenna << " \n";
     std::cout << " adresa promenne = " << &promenna << " \n";
     return 0;
